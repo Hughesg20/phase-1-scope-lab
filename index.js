fe47ed31bea92b6fcd7b42a6b92d63071e7608bc
Griffin Hughes <Griffinhughes2020@gmail.com>
2021-12-14 19:27:20 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+var upperCaseCustomerName=()=>{customerName=customerName.toUpperCase()};
+
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+  return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+  }
+
+  const leastFavoriteCustomer = '';
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = '';
+    return leastFavoriteCustomer;
+  }
